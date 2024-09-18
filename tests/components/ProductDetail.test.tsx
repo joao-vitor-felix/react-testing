d302ce3e9ef11a9bb764ddfcb2817683f31fefc1
João Vitor <joaovitorfelixcontato@gmail.com>
2024-09-18 14:07:01 -0300

    test: refactor ProductDetail

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -2,16 +2,25 @@ import { render, screen } from '@testing-library/react'
 import ProductDetail from '../../src/components/ProductDetail'
 import { server } from '../mocks/server'
 import { http, HttpResponse } from 'msw'
-import { products } from '../mocks/data'
+import { db } from '../mocks/db'
 
 describe('ProductDetail', () => {
+  let productId: number;
+
+  beforeAll(() => {
+    const product = db.product.create();
+    productId = product.id
+  });
+
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId } } });
+  })
+
   it('should render product detail', async () => {
-    render(<ProductDetail productId={1} />)
-
-    const [product] = products
-
-    expect(await screen.findByText(product.name, { exact: false })).toBeInTheDocument()
-    expect(await screen.findByText(`$${product.price}`, { exact: false })).toBeInTheDocument()
+    const product = db.product.findFirst({ where: { id: { equals: productId }}})
+    render(<ProductDetail productId={productId} />)
+    expect(await screen.findByText(product!.name, { exact: false })).toBeInTheDocument()
+    expect(await screen.findByText(`$${product!.price}`, { exact: false })).toBeInTheDocument()
   })
 
   it('should render loading message initially', () => {
@@ -24,12 +33,21 @@ describe('ProductDetail', () => {
     expect(screen.getByText(/invalid/i)).toBeInTheDocument()
   })  
 
+  it('should render error message when fetch failed', async () => {
+    server.use(
+      http.get('/products/:id', async () => {
+        return HttpResponse.error()
+      }))
+    render(<ProductDetail productId={24121} />)
+    expect(await screen.findByText(/error/i)).toBeInTheDocument()
+  })  
+
   it('should render not found message when product is not found', async () => {
     server.use(
     http.get('/products/:id', async () => {
       return HttpResponse.json(null, { status: 404 })
     }))
-    render(<ProductDetail productId={1} />)
+    render(<ProductDetail productId={24121} />)
     expect(await screen.findByText(/not found/i)).toBeInTheDocument()
   })  
 })
--- tests/mocks/data.ts
+++
@@ -1,7 +0,0 @@
-import { fakerPT_PT as faker } from '@faker-js/faker';
-
-export const products = [
-  { id: 1, name: faker.commerce.productName(), price: faker.commerce.price({min: 20, max: 800 }) },
-  { id: 2, name: faker.commerce.productName(), price: faker.commerce.price({min: 20, max: 800 }) },
-  { id: 3, name: faker.commerce.productName(), price: faker.commerce.price({min: 20, max: 800 }) },
-];
