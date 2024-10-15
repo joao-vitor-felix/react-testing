f04a64b37b6470eff75b24f1e9c78c2b5efa57f5
João Vitor <joaovitorfelixcontato@gmail.com>
2024-10-15 17:19:38 -0300

    refactor BrowseProducts

--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -1,13 +1,12 @@
-import { Theme } from "@radix-ui/themes";
 import {
   render,
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import BrowseProducts from "../../src/pages/BrowseProductsPage";
 import userEvent from "@testing-library/user-event";
 import { Category, Product } from "../../src/entities";
-import { CartProvider } from "../../src/providers/CartProvider";
+import BrowseProducts from "../../src/pages/BrowseProductsPage";
+import Providers from "../../src/providers";
 import { db, getProductsByCategory } from "../mocks/db";
 import { simulateDelay, simulateError } from "../utils";
 
@@ -16,19 +15,21 @@ describe("BrowseProductsPage", () => {
   const products: Product[] = [];
 
   beforeAll(() => {
-    [1, 2].forEach((item) => {
-      const category = db.category.create({ name: `Category ${item}` });
+    [1, 2].forEach(() => {
+      const category = db.category.create();
       categories.push(category);
       [1, 2].forEach(() => {
         products.push(db.product.create({ categoryId: category.id }));
       });
     });
   });
+
   afterAll(() => {
     const categoryIds = categories.map((c) => c.id);
     db.category.deleteMany({
       where: { id: { in: categoryIds } },
     });
+
     const productIds = products.map((p) => p.id);
     db.product.deleteMany({
       where: { id: { in: productIds } },
@@ -36,13 +37,7 @@ describe("BrowseProductsPage", () => {
   });
 
   const renderComponent = () => {
-    render(
-      <CartProvider>
-        <Theme>
-          <BrowseProducts />
-        </Theme>
-      </CartProvider>
-    );
+    render(<BrowseProducts />, { wrapper: Providers });
 
     const getCategoriesSkeleton = () =>
       screen.queryByRole("progressbar", {
@@ -57,11 +52,10 @@ describe("BrowseProductsPage", () => {
     const selectCategory = async (name: RegExp | string) => {
       await waitForElementToBeRemoved(getCategoriesSkeleton);
       const combobox = getCategoriesComboBox();
-      const user = userEvent.setup();
-      await user.click(combobox!);
+      await userEvent.click(combobox!);
 
       const option = screen.getByRole("option", { name });
-      await user.click(option);
+      await userEvent.click(option);
     };
 
     const expectProductsToBeInTheDocument = (products: Product[]) => {
@@ -85,46 +79,61 @@ describe("BrowseProductsPage", () => {
 
   it("should show a loading skeleton when fetching categories", () => {
     simulateDelay("/categories");
+
     const { getCategoriesSkeleton } = renderComponent();
+
     expect(getCategoriesSkeleton()).toBeInTheDocument();
   });
 
   it("should hide the loading skeleton after categories are fetched", async () => {
     const { getCategoriesSkeleton } = renderComponent();
-    await waitForElementToBeRemoved(() => getCategoriesSkeleton());
+
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
   });
 
   it("should show a loading skeleton when fetching products", () => {
     simulateDelay("/products");
+
     const { getProductsSkeleton } = renderComponent();
+
     expect(getProductsSkeleton()).toBeInTheDocument();
   });
 
   it("should hide the loading skeleton after products are fetched", async () => {
     const { getProductsSkeleton } = renderComponent();
+
     await waitForElementToBeRemoved(getProductsSkeleton);
   });
 
   it("should not render an error if categories cannot be fetched", async () => {
     simulateError("/categories");
+
     const { getCategoriesSkeleton, getCategoriesComboBox } = renderComponent();
+
     await waitForElementToBeRemoved(getCategoriesSkeleton);
+
     expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
     expect(getCategoriesComboBox()).not.toBeInTheDocument();
   });
 
   it("should render an error if products cannot be fetched", async () => {
     simulateError("/products");
+
     renderComponent();
+
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 
   it("should render categories", async () => {
     const { getCategoriesSkeleton, getCategoriesComboBox } = renderComponent();
+
     await waitForElementToBeRemoved(getCategoriesSkeleton);
+
     const combobox = getCategoriesComboBox();
     expect(combobox).toBeInTheDocument();
+
     await userEvent.click(combobox!);
+
     expect(screen.getByRole("option", { name: /all/i })).toBeInTheDocument();
     categories.forEach((category) => {
       expect(
@@ -135,7 +144,9 @@ describe("BrowseProductsPage", () => {
 
   it("should render products", async () => {
     const { getProductsSkeleton } = renderComponent();
+
     await waitForElementToBeRemoved(getProductsSkeleton);
+
     products.forEach((product) => {
       expect(screen.getByText(product.name)).toBeInTheDocument();
     });
