e823be54e0acdc1c95aa8544c1478c2cdb79e699
João Vitor <joaovitorfelixcontato@gmail.com>
2024-10-24 08:53:31 -0300

    add AuthStatus test

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,24 +1,24 @@
-import '@testing-library/jest-dom/vitest';
-import ResizeObserver from 'resize-observer-polyfill';
-import { server } from './mocks/server';
+import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+import { server } from "./mocks/server";
 
 beforeEach(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
+vi.mock("@auth0/auth0-react");
 global.ResizeObserver = ResizeObserver;
-
 window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
-Object.defineProperty(window, 'matchMedia', {
+Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation(query => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: vi.fn(), 
+    addListener: vi.fn(),
     removeListener: vi.fn(),
     addEventListener: vi.fn(),
     removeEventListener: vi.fn(),
